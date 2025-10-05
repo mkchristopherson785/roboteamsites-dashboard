7d13b586f204e9a28956b46e8b3412ca7250d36b
Matt Christopherson <matt@arcwavestudios.com>
2025-10-05 11:38:12 -0400

    Auth: remove /auth/cb page; keep server route only

--- app/auth/cb/page.tsx
+++
@@ -1,5 +0,0 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-export const fetchCache = 'force-no-store'
-import ClientCallback from './Client'
-export default function Page() { return <ClientCallback /> }
